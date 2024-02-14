e17a50db4cb0ccc103b959c512ff127be6d3cbea
silas <silasgebhart12@gmail.com>
2024-02-13 20:33:04 -0600

    final commit

---
+++ index.js
@@ -0,0 +1,11 @@
+const divide = (parameter1, parameter2) => parameter1 / parameter2;
+
+divide(2000, 100);
+
+const add = (parameter1, parameter2) => parameter1 + parameter2;
+
+add(100, 167);
+
+const square = (x) => x * x;
+
+square(5);
